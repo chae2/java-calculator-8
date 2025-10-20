a47f6fa4a26052916e9a70c436e962a48c8a0807
chae2 <saintcharry22@gmail.com>
2025-10-20 23:26:51 +0900

    feat: add illegalArgumentException & edge cases coverage

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,5 +1,6 @@
 package calculator;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
@@ -9,14 +10,35 @@ public class Application {
         // get string
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = sc.nextLine();
+        if (input.contains("//") && input.contains("\\n")){
+            int idx = input.indexOf("//");
+            Character custom = input.charAt(idx+2);
+//            System.out.println("custom: "+custom);
+
+            String regex_custom = "[0-9:,"+custom+"]";
+//            System.out.println("regex_custom: "+regex_custom);
+            String regex_cond = "^"+regex_custom+"*(//.\\\\n)?"+regex_custom+"*$";
+//            System.out.println("regex_cond: "+regex_cond);
+            if (!input.matches(regex_cond)) {
+                System.out.println("input: "+input);
+                throw new IllegalArgumentException();
+            }
+        } else {
+            String regex_cond = "^[0-9:,]*$";
+            if (!input.matches(regex_cond)) throw new IllegalArgumentException();
+        }
 
         String regex_ = ",|:";
         if (input.contains("//") && input.contains("\\n") && input.indexOf("\\n") == input.indexOf("//")+3){
             int idx = input.indexOf("//");
             Character custom = input.charAt(idx+2);
-            regex_ += "|"+custom;
+//            System.out.println("custom: "+custom);
+            String escapedCustom = Pattern.quote(String.valueOf(custom));
+            regex_ += "|"+escapedCustom;
             input = input.replace("\\n", "").replace("//","");
         }
+//        System.out.println("input: "+input);
+//        System.out.println("split regex: " + regex_);
 
         String tokens[] = input.split(regex_);
         int sum = 0;
