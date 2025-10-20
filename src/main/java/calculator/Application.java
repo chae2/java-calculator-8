8ac2ba7aaf2c3c60300cd436009ebbc28230cba6
chae2 <saintcharry22@gmail.com>
2025-10-20 21:42:37 +0900

    Revert "feat: update application.java"
    This reverts commit 990ab63b14f99b454192917a5d4fc5fbb4b7ce50.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,29 +1,7 @@
 package calculator;
-import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Scanner sc = new Scanner(System.in);
-
-        // get string
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
-        String input = sc.nextLine();
-
-        String regex_ = ",|:";
-        if (input.contains("//") && input.contains("\\n") && input.indexOf("\\n") == input.indexOf("//")+3){
-            int idx = input.indexOf("//");
-            Character custom = input.charAt(idx+2);
-            regex_ += "|"+custom;
-            input = input.replace("\\n", "").replace("//","");
-        }
-
-        String tokens[] = input.split(regex_);
-        int sum = 0;
-        for (String token : tokens) {
-//            System.out.println("token: " + token);
-            if (!token.isEmpty() && Character.isDigit(token.charAt(0))) sum += Character.getNumericValue(token.charAt(0));
-        }
-        System.out.println("결과 : " + sum);
     }
 }
